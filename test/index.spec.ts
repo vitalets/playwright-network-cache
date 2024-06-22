78f567a3d66c95033729049206e165b0b8f387da
Vitaliy Potapov <noginsk@rambler.ru>
2024-06-22 12:25:46 +0400

    test: check files on fs

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,12 +1,17 @@
 import { test, expect } from '@playwright/test';
+import fs from 'node:fs';
 import { routeWithCache, fetchWithCache } from '../src';
 import type { Cat } from './server';
+
+const cacheDir = process.env.NETWORK_CACHE_DIR;
 
 test('routeWithCache', async ({ page }) => {
   await page.goto('/');
   await routeWithCache(page, '/api/cats');
   await page.getByRole('button', { name: 'Fetch Cats' }).click();
   await expect(page.getByRole('list')).toContainText('Whiskers');
+  expect(json(`${cacheDir}/localhost/api-cats/GET/headers.json`)).toHaveProperty('status', 200);
+  expect(json(`${cacheDir}/localhost/api-cats/GET/body.json`)).toHaveProperty('[0].id', 1);
 });
 
 test('routeWithCache (custom key)', async ({ page }) => {
@@ -16,6 +21,7 @@ test('routeWithCache (custom key)', async ({ page }) => {
   });
   await page.getByRole('button', { name: 'Fetch Cats' }).click();
   await expect(page.getByRole('list')).toContainText('Whiskers');
+  expect(json(`${cacheDir}/custom-key/GET/headers.json`)).toHaveProperty('status', 200);
 });
 
 test('routeWithCache (context)', async ({ page, context }) => {
@@ -41,7 +47,7 @@ test('fetchWithCache (custom key)', async ({ page }) => {
   await page.goto('/');
   await page.route('/api/cats', async (route) => {
     const res = await fetchWithCache(route, null, {
-      cacheKey: 'cats',
+      cacheKey: 'custom-key2',
     });
     const json: Cat[] = await res.json();
     json[0].name = 'Kitty';
@@ -49,4 +55,9 @@ test('fetchWithCache (custom key)', async ({ page }) => {
   });
   await page.getByRole('button', { name: 'Fetch Cats' }).click();
   await expect(page.getByRole('list')).toContainText('Kitty');
+  expect(json(`${cacheDir}/custom-key2/headers.json`)).toHaveProperty('status', 200);
 });
+
+function json(filePath: string) {
+  return JSON.parse(fs.readFileSync(filePath, 'utf8'));
+}
